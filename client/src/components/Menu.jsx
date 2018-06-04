6b618c57bd19b48a2aa0b07e7a637683e7f3c044
Anoop Sownderraj <asownder95@gmail.com>
2018-06-03 21:29:02 -0700

    removing node_modules

--- client/src/components/Menu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Menu = (props) => (
-  <div>
-    This is a React component!
-  </div>
-);
-
-export default Menu;
--- client/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Menu from './components/Menu.jsx';
-
-ReactDOM.render(<Menu />, document.getElementById('menu_module'));
